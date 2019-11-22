e33587f4943d9c6ab2d9a2c417ea1ed8a4a2f116
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2019-11-22 12:01:11 +0900

    add method

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/cg-matsuno/sqlboiler-goose-sample
 go 1.13
 
 require (
+	github.com/cg-matsuno/sqlboiler-goose-sample/db v0.0.0-20191122024004-a3639e85e772
 	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/lib/pq v1.1.1
 	github.com/mattn/go-colorable v0.1.4 // indirect
-	github.com/volatiletech/inflect v0.0.0-20170731032912-e7201282ae8d // indirect
+	github.com/volatiletech/null v8.0.0+incompatible
 	github.com/volatiletech/sqlboiler v3.6.1+incompatible
 )
--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 
 	dbmodels "github.com/cg-matsuno/sqlboiler-goose-sample/db/models"
 	"github.com/k0kubun/pp"
+	_ "github.com/lib/pq"
+	"github.com/volatiletech/null"
 	"github.com/volatiletech/sqlboiler/boil"
+	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 func init() {
@@ -18,14 +21,16 @@ func init() {
 	}
 	boil.SetDB(_db)
 	boil.SetLocation(time.Local)
-	return nil
 }
 
 func main() {
 	var err error
 	ctx := context.Background()
 
-	if err = insertUsers(ctx); err != nil {
+	if err = deleteAll(ctx); err != nil {
+		panic(err)
+	}
+	if err = insertUser(ctx); err != nil {
 		panic(err)
 	}
 
@@ -38,50 +43,106 @@ func main() {
 	}
 }
 
-func insertUser(ctx) (err error) {
-	user := dbmodels.User{}
-	if users, err = dbmodels.Users().One(ctx, tx); err != nil {
+func insertUser(ctx context.Context) (err error) {
+	var tx boil.ContextTransactor
+	if tx, err = boil.BeginTx(ctx, nil); err != nil {
 		return err
 	}
-	pp.Println(users)
 
-	var users dbmodels.UserSlice
-	if users, err = dbmodels.Users(
-		dbmodels.UserWhere.ID.EQ(1),
-	).AllG(ctx, tx); err != nil {
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+	org := dbmodels.Org{
+		Name: "org name",
+	}
+	if err = org.Insert(ctx, tx, boil.Infer()); err != nil {
 		return err
 	}
-	pp.Println(users)
+
+	user := dbmodels.User{
+		OrgID: org.ID,
+		UID:   null.StringFrom("uid"),
+		Name:  "user name",
+		Owner: true,
+	}
+
+	if err = user.Insert(ctx, tx, boil.Infer()); err != nil {
+		return err
+	}
+	return nil
 }
 
-func selectUsers(ctx) (err error) {
+func selectUsers(ctx context.Context) (err error) {
 	var users dbmodels.UserSlice
-	if users, err = dbmodels.Users().AllG(ctx, tx); err != nil {
+	if users, err = dbmodels.Users().AllG(ctx); err != nil {
 		return err
 	}
-	pp.Println(users)
+	for _, u := range users {
+		pp.Println("User Name", u.Name)
+	}
 
-	var users dbmodels.UserSlice
-	if users, err = dbmodels.Users(
-		dbmodels.UserWhere.ID.EQ(1),
-	).AllG(ctx, tx); err != nil {
+	var usersWithWhere dbmodels.UserSlice
+	if usersWithWhere, err = dbmodels.Users(
+		dbmodels.UserWhere.Name.EQ("user name"),
+	).AllG(ctx); err != nil {
 		return err
 	}
-	pp.Println(users)
+	for _, u := range usersWithWhere {
+		pp.Println("User Name", u.Name)
+	}
+	return nil
 }
 
-func selectOneUser(ctx) (err error) {
-	var users *dbmodels.User
-	if users, err = dbmodels.Users().One(ctx, tx); err != nil {
+func selectOneUser(ctx context.Context) (err error) {
+	var user *dbmodels.User
+	if user, err = dbmodels.Users(
+		dbmodels.UserWhere.Name.EQ("user name"),
+		qm.Load(dbmodels.UserRels.Org),
+	).OneG(ctx); err != nil {
+		if err == sql.ErrNoRows {
+			pp.Println("no user found")
+			return nil
+		}
 		return err
 	}
-	pp.Println(users)
+	pp.Println("User Name", user.Name)
+	pp.Println("Org Name", user.R.Org.Name)
+	return nil
+}
 
-	var users dbmodels.UserSlice
-	if users, err = dbmodels.Users(
-		dbmodels.UserWhere.ID.EQ(1),
-	).AllG(ctx, tx); err != nil {
+func deleteAll(ctx context.Context) (err error) {
+	var tx boil.ContextTransactor
+	if tx, err = boil.BeginTx(ctx, nil); err != nil {
 		return err
 	}
-	pp.Println(users)
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	var orgs dbmodels.OrgSlice
+	if orgs, err = dbmodels.Orgs(
+		qm.Load(dbmodels.OrgRels.Users),
+	).AllG(ctx); err != nil {
+		return err
+	}
+
+	for _, org := range orgs {
+		if _, err = org.R.Users.DeleteAll(ctx, tx); err != nil {
+			return err
+		}
+
+		if _, err = org.Delete(ctx, tx); err != nil {
+			return err
+		}
+	}
+	return nil
 }
